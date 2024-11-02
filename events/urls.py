2e3be023ab90bee0a7a47f1c700b86a8d4b999eb
Macedonf <matheus.joaonf@gmail.com>
2024-11-02 14:56:02 -0300

    fase02

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,12 @@
+
 from django.contrib import admin
 from django.urls import path
 from events import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('eventos/', views.list_events, name='list_events'),
+
+    path('<int:evento_id>/', views.detalhe_evento, name='detalhe_evento'),
 
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from events.models import Evento
 
@@ -8,4 +8,12 @@ def index(request):
     return render(request, 'events/index.html')
 
 
+#listar eventos
 
+def list_events(request):
+    eventos = Evento.objects.all()
+    return render(request, 'events/list_events.html', {'eventos': eventos})
+
+def detalhe_evento(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+    return render(request, 'events/evento_detalhado.html', {'evento': evento})
